22e296ffdb130e577ec53664f3b2fbc330b49d41
Bernardo Dias <55239586+joaobagas@users.noreply.github.com>
2021-05-20 21:48:58 +0100

    Final commit

--- app/src/main/java/com/joaob/emergencyinbabel/data/api/PhraseAPI.java
+++ app/src/main/java/com/joaob/emergencyinbabel/data/api/PhraseAPI.java
@@ -7,6 +7,7 @@ import java.util.List;
 
 import retrofit2.Call;
 import retrofit2.http.GET;
+import retrofit2.http.Query;
 
 public interface PhraseAPI {
     @GET("phrases")
--- app/src/main/java/com/joaob/emergencyinbabel/data/repository/PhraseRepository.java
+++ app/src/main/java/com/joaob/emergencyinbabel/data/repository/PhraseRepository.java
@@ -18,13 +18,13 @@ import java.util.List;
 import retrofit2.Call;
 import retrofit2.Callback;
 import retrofit2.Response;
-import retrofit2.internal.EverythingIsNonNull;
 
 public class PhraseRepository {
     private static PhraseRepository instance;
     private PhraseAPI phraseAPI;
     private RecyclerView phraseRecyclerView;
     private PhraseListAdapter phraseListAdapter;
+    private String languageID;
 
     private MutableLiveData<List<Phrase>> phrases;
     public MutableLiveData<List<Phrase>> getPhraseListObserver() { return phrases; }
@@ -47,31 +47,41 @@ public class PhraseRepository {
 
     public void updatePhrases(String languageID) {
         Call<List<Phrase>> call = phraseAPI.getPhrases();
-        call.enqueue(new Callback<List<Phrase>>() {
-            @Override
-            public void onResponse(Call<List<Phrase>> call, Response<List<Phrase>> response) {
-                if(response.code() == 200) {
-                    phrases.postValue(response.body());
-                    if(phrases.getValue() != null) {
-                        ArrayList<Phrase> phrasesTemp = (ArrayList<Phrase>) phrases.getValue();
-                        ArrayList<Phrase> returnablePhrases = new ArrayList<>();
-                        for(int i = 0; i < phrasesTemp.size(); i++) {
-                            if(phrasesTemp.get(i).getLanguageID().equals(languageID)) {
-                                returnablePhrases.add(phrasesTemp.get(i));
-                            }
-                        }
-                        phraseListAdapter = new PhraseListAdapter(returnablePhrases);
-                        phraseRecyclerView.setAdapter(phraseListAdapter);
-                    }
-                } else {
-                    Log.i("Retrofit", "Something went wrong :(");
-                }
-            }
+        ApiCallback apiCallback = new ApiCallback();
+        apiCallback.setLanguageID(languageID);
+        call.enqueue(apiCallback);
+    }
 
-            @Override
-            public void onFailure(Call<List<Phrase>> call, Throwable t) {
+    private class ApiCallback implements Callback<List<Phrase>> {
+        String languageID;
+
+        public void setLanguageID(String languageID) {
+            this.languageID = languageID;
+        }
+
+        @Override
+        public void onResponse(Call<List<Phrase>> call, Response<List<Phrase>> response) {
+            if(response.code() == 200) {
+                phrases.postValue(response.body());
+                if(phrases.getValue() != null) {
+                    ArrayList<Phrase> phrasesTemp = (ArrayList<Phrase>) phrases.getValue();
+                    ArrayList<Phrase> returnablePhrases = new ArrayList<>();
+                    for(int i = 0; i < phrasesTemp.size(); i++) {
+                        if (phrasesTemp.get(i).getLanguageID().equals("bu")) {
+                            returnablePhrases.add(phrasesTemp.get(i));
+                        }
+                    }
+                    phraseListAdapter = new PhraseListAdapter(returnablePhrases);
+                    phraseRecyclerView.setAdapter(phraseListAdapter);
+                }
+            } else {
                 Log.i("Retrofit", "Something went wrong :(");
             }
-        });
+        }
+
+        @Override
+        public void onFailure(Call<List<Phrase>> call, Throwable t) {
+            Log.i("Retrofit", "Something went wrong :(");
+        }
     }
 }
